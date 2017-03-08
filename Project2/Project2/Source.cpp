4e4ebfb76c7e11bc70ca2bfd2290ef8b5ac67297
game110pm <game110pm@605-110-STU05>
2017-03-08 13:10:30 -0700

    vm

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int counter = 0;
+	char input = 'b';
+	while (input == 'b') {//game loop
+		cin >> input;
+		if (input == 'b')
+			counter += 1;
+	}
+	cout << counter;
+
+
+
+
+}
